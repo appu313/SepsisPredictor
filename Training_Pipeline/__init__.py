a901d59c42ef1af9289aa55ad76741316ad8ae11
Mitchell Teunissen <mitchteun@gmail.com>
2025-07-02 17:16:53 -0400

    add hyperparam tuning sections to notebook

--- Training_Pipeline/__init__.py
+++ Training_Pipeline/__init__.py
@@ -7,7 +7,8 @@ Each module is owned by a specific team member for clear responsibility.
 Modules and owners:
 -------------------
 - train.py            --> Responsible: Mitch (Training loop and DataLoader initialization)
-- evaluate.py         --> Responsible: Ehsan (Implement evaluation metrics (AUROC, PRC, etc.))
+- tune.py             --> Responsible: Mitch (Grid search hyperparameter tuning)
+- evaluate.py         --> Responsible: _____ (Implement evaluation metrics (AUROC, PRC, etc.))
 - visualize.py        --> Responsible: _____ (Logging & experiment tracking (TensorBoard))
 """
 from .train import train, Train_Hyperparameter_Grid, Train_Hyperparameters
