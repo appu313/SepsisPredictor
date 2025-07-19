14269e33520a16da93e904bcf68b243a5ed3a938
Mitchell Teunissen <mitchteun@gmail.com>
2025-07-19 13:07:49 -0400

    return fpr and tpr values to allow for roc curve plotting

--- Training_Pipeline/evaluate.py
+++ Training_Pipeline/evaluate.py
@@ -6,6 +6,20 @@ from tqdm import tqdm
 
 
 def roc_eval(model, dataset: TensorDataset, batch_size=32, quiet=False):
+    """Evaluates the model on the provided test dataset using the ROC curve
+
+    Args:
+        model (torch.nn.Module): Untrained model, initialized with desired hyperparameters
+        dataset (TensorDataset): Test dataset
+        batch_size (int, optional): Data batch size. Defaults to 32.
+        quiet (bool, optional): When true, disables progress bars. Defaults to False.
+
+    Returns:
+        Tuple containing
+        - auroc (float): The area under the ROC curve
+        - best_threshold (float): The probability threshold that maximizes the Youden J statistic
+        - roc_curve (tuple[ndarray, ndarray]): Tuple of false positive rates and true positive rates for plotting the ROC curve
+    """
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -27,8 +41,9 @@ def roc_eval(model, dataset: TensorDataset, batch_size=32, quiet=False):
             all_preds.extend(probs.cpu().numpy())
     fpr, tpr, thresholds = roc_curve(all_labels, all_preds)
     auroc = auc(fpr, tpr)
+    
 
     # maximize the Youden J statistic to ensure best class separation
     best_threshold = thresholds[np.argmax(tpr - fpr)]
     
-    return auroc, best_threshold
+    return auroc, best_threshold, (fpr, tpr)
